52671a82006c8f4a2d28fd0ca605e8adae6354c6
Ribaas <ribasleonardo92@gmail.com>
2025-10-29 17:31:57 -0300

    test: criando teste para FreteStrategyResolver

---
+++ Frete.Tests/FreteStrategyResolverTests.cs
@@ -0,0 +1,34 @@
+using Frete.Application.Strategies;
+using Frete.Domain.Enums;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Frete.Tests;
+
+public class FreteStrategyResolverTests
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly FreteStrategyResolver _freteStrategyResolver;
+    
+    public FreteStrategyResolverTests()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        services.AddScoped<NormalFreteStrategy>();
+        services.AddScoped<ExpressaFreteStrategy>();
+        services.AddScoped<AgendadaFreteStrategy>();
+        
+        _serviceProvider = services.BuildServiceProvider();
+        _freteStrategyResolver = new FreteStrategyResolver(_serviceProvider);
+    }
+
+    [Fact]
+    public void Resolver_deve_retornar_NormalFreteStrategy()
+    {
+        // Act
+        var strategy = _freteStrategyResolver.Resolve(ModalidadeFrete.Normal);
+        
+        // Assert
+        Assert.NotNull(strategy);
+        Assert.IsType<NormalFreteStrategy>(strategy);
+    }
+}
