b33aa423bd66e36351a14433f31941c6b54805f5
Ribaas <ribasleonardo92@gmail.com>
2025-10-29 16:36:30 -0300

    test: criando teste para strategy de calculo de frete normal

---
+++ Frete.Tests/FreteStrategyTests.cs
@@ -0,0 +1,22 @@
+namespace Frete.Tests;
+
+public class FreteStrategyTests
+{
+    [Fact]
+    public void Deve_calcular_frete_normal()
+    {
+        // Arrange
+        var strategy = new NormalFreteStrategy();
+        // parametros = peso, distancia, taxa fixa
+        var parametros = new FreteParametros(5m, 10m, 2m);
+        
+        // Act
+        var valor = strategy.Calcular(parametros);
+        
+        // Assert
+        // Calculo Frete Normal = peso * 0.5 + distancia * 0.1 + taxaFixa
+        var esperado = (5m * 0.5m) + (10m * 0.1m) + 2m;
+        Assert.Equal(esperado, valor);
+    }
+    
+}
