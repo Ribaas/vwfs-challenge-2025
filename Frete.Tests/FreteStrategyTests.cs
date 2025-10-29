8ed7422eff4c832578c793c013eef732a6086d86
Ribaas <ribasleonardo92@gmail.com>
2025-10-29 17:03:55 -0300

    test: criando teste para strategy de calculo de frete expresso

--- Frete.Tests/FreteStrategyTests.cs
+++ Frete.Tests/FreteStrategyTests.cs
@@ -17,8 +17,25 @@ public class FreteStrategyTests
         var valor = strategy.CalcularFrete(parametros);
         
         // Assert
-        // Calculo Frete Normal = peso * 0.5 + distancia * 0.1 + taxaFixa
+        // Calculo Frete Normal = peso * 0.5 + distancia * 0.1 + taxaFixa (2)
         var esperado = (5m * 0.5m) + (10m * 0.1m) + 2m;
+        Assert.Equal(esperado, valor);
+    }
+    
+    [Fact]
+    public void Deve_calcular_frete_expresso()
+    {
+        // Arrange
+        var strategy = new ExpressaFreteStrategy();
+        // parametros = peso, distancia, taxa fixa
+        var parametros = new FreteParametros(5m, 10m, 2m);
+        
+        // Act
+        var valor = strategy.CalcularFrete(parametros);
+        
+        // Assert
+        // Calculo Frete Expresso = peso * 0.5 + distancia * 1 + taxaFixa (5)
+        var esperado = (5m * 0.5m) + (10m * 1m) + 5m;
         Assert.Equal(esperado, valor);
     }
     
